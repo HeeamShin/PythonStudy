3494f2599ccd97ca8c16161b0289f3b7684bde07
HeeamShin <ska900222@gmail.com>
2020-04-08 21:50:22 +0900

    Studied numbers and operators

---
+++ CH02/2-2-number.py
@@ -0,0 +1,24 @@
+a = 1       # integer
+b = 3.2     # floating point
+
+# float with exponentiation
+c = 5.273e2
+print(c)
+d = 5.273e-2
+print(d)
+
+# arithmetic operators
+addition_sample = 5 + 7
+subtraction_sample = 5 - 7
+multiplication_sample = 5 * 7
+division_sample = 5 / 7
+
+# additional operators
+floor_division_sample = 5 // 7
+modulus_sample = 7 % 5
+exponentiation_sample = 5 ** 7
+
+# operation priority
+print("Hello" + "World!" * 3)
+print(2 + 2 - 2 * 2 / 2 * 2)
+print(2 - 2 + 2 / 2 * 2 + 2)
